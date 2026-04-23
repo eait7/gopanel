fabe2047eedb1dac47350b4349cd5c474248fd4d
GoPanel Admin <admin@gopanel.local>
2026-04-23 17:36:29 +0200

    chore: remove diagnostic script

--- dtest.go
+++
@@ -1,4 +0,0 @@
-package main
-import "os/exec"
-import "fmt"
-func main(){ out, err := exec.Command("docker", "cp", "gopanel-dashboard:/app/data", "/tmp/data2").CombinedOutput(); fmt.Println(string(out), err) }
